b9c48ff9a3bcf29e2f5b85470159b8619b7eaef1
datjul <datjul@taltech.ee>
2026-03-12 22:07:15 +0200

    added regex for cloudflare cors

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,14 @@ import { join } from 'node:path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(process.cwd(), 'public'));
-  const origin = ['http://localhost:8081'];
+  const origin: Array<string | RegExp> = ['http://localhost:8081'];
+
   if (process.env.CLIENT_PUBLIC_API_URL) {
     origin.push(process.env.CLIENT_PUBLIC_API_URL);
   }
+
+  const cloudflareRegex = /^https?:\/\/([a-z0-9]+)\.wwwclient\.pages\.dev$/;
+  origin.push(cloudflareRegex);
   app.enableCors({
     origin: origin,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
